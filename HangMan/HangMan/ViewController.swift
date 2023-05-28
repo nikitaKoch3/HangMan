7c8625bcffc4c497664b4ef09a84d656d2b6ed41
Nikita Kochubey <nikitakochubey@Nikitas-iMac.local>
2023-05-28 10:45:19 -0400

    Projects was added

---
+++ HangMan/HangMan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HangMan
+//
+//  Created by Nikita Kochubey on 5/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
