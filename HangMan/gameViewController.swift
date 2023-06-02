8068c8d346b9cc228a0eb9bf3922a9d8421e9194
Nikita Kochubey <nikitakochubey@Nikitas-iMac.local>
2023-06-01 20:04:58 -0400

    Greetings Screen and Menu Screen was changed

---
+++ HangMan/gameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  gameViewController.swift
+//  HangMan
+//
+//  Created by Nikita Kochubey on 5/28/23.
+//
+
+import UIKit
+
+class gameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HangMan/greetingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  greetingsViewController.swift
+//  HangMan
+//
+//  Created by Nikita Kochubey on 6/1/23.
+//
+
+import UIKit
+
+class greetingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
