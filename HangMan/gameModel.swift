c032c7e4fd81a8313d011255cc98d2de84e62386
Nikita Kochubey <nikitakochubey@Nikitas-iMac.local>
2023-06-11 12:52:50 -0400

    Added a new model

---
+++ HangMan/gameModel.swift
@@ -0,0 +1,70 @@
+//
+//  model.swift
+//  HangMan
+//
+//  Created by Nikita Kochubey on 6/11/23.
+//
+
+import Foundation
+
+class Game {
+    
+    var words: [String] = ["plane", "dog", "building", "ocean", "mercury", "mountain", "space", "california", "laptop", "school", "cat"]
+    
+    var tries: Int = 6
+    
+    var chosenWords: String = "moon"
+    
+    var word_completion: String = "_________"
+    
+    func chooseRandomWord() -> String {
+        let index = Int.random(in: 0..<words.count)
+        
+        return words[index]
+        
+    }
+    
+    func createCompletion(chosenWord: String) -> String {
+        var completion = ""
+        for _ in 1...chosenWord.count {
+            completion += "_"
+        }
+        return completion
+    }
+    
+    func openLetter(completion: String, word: String, letter: Character) -> String {
+            var new_completion = ""
+        
+            let index = word.firstIndex(of: letter)
+        
+            for i in completion.indices {
+                if i == index || word[i] == letter {
+                    new_completion += String(letter)
+                }
+                else if completion[i] != "_" {
+                        new_completion += String(completion[i])
+                    }
+                                      else {
+                    new_completion += "_"
+                }
+            }
+                    return new_completion
+                }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                }
--- HangMan/gameViewController.swift
+++ HangMan/gameViewController.swift
@@ -5,25 +5,25 @@
 //  Created by Nikita Kochubey on 5/28/23.
 //
 
+
 import UIKit
 
 class gameViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var tries: UILabel!
+    @IBOutlet weak var wordCompletion: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didCheck(_ sender: Any) {
     }
-    */
+    
 
 }
