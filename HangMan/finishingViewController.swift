b9c9f6c9e0c94629eadbed2091338dbd425a9b84
Nikita Kochubey <nikitakochubey@Nikitas-iMac.local>
2023-06-29 13:02:50 -0400

    added the finishing view controller

---
+++ HangMan/finishingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  finishingViewController.swift
+//  HangMan
+//
+//  Created by Nikita Kochubey on 6/29/23.
+//
+
+import UIKit
+
+class finishingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
